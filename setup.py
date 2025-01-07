d19d6900d5d96dd32dff6942025eab493d176d9b
preetiaditi <preeti.sahay5182@gmail.com>
2025-01-07 18:09:31 +0800

    Basic Structure Done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Preeti Sahay",
+    author_email="preeti.sahay5182@gmail.com",
+    packages=find_packages()
+)
